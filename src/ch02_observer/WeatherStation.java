628e5087f8afe7ea73034c40cc0f4ebae57d19f5
Jinpyo-Ahn <dkswlsvy3312@gmail.com>
2025-01-16 19:33:36 +0900

    Chapter02

---
+++ src/ch02_observer/WeatherStation.java
@@ -0,0 +1,10 @@
+package ch02_observer;
+
+public class WeatherStation {
+
+    public static void main(String[] args) {
+        WeatherData weatherData = new WeatherData();
+
+        CurrentConditionDisplay currentDisplay = new CurrentConditionDisplay()
+    }
+}
