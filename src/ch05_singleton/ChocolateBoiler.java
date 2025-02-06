5606c37026535b84c1caffe6d120f0fdd44f5fe2
Jinpyo-Ahn <dkswlsvy3312@gmail.com>
2025-02-06 20:00:57 +0900

    Chapter05 Singleton

---
+++ src/ch05_singleton/ChocolateBoiler.java
@@ -0,0 +1,39 @@
+package ch05_singleton;
+
+public class ChocolateBoiler {
+
+    private boolean empty;
+    private boolean boiled;
+
+    private ChocolateBoiler(boolean empty, boolean boiled) {
+        this.empty = empty;
+        this.boiled = boiled;
+    }
+
+    public void fill() {
+        if (isEmpty()) {
+            empty = false;
+            boiled = false;
+        }
+    }
+
+    public void drain() {
+        if (!isEmpty() && isBoiled()) {
+            empty = true;
+        }
+    }
+
+    public void boil() {
+        if (!isEmpty() && !isBoiled()) {
+            boiled = true;
+        }
+    }
+
+    public boolean isEmpty() {
+        return empty;
+    }
+
+    public boolean isBoiled() {
+        return boiled;
+    }
+}
